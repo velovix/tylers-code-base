393f9cdb4abf157ba975ac5cf6e9efaa58cfc966
Tyler Compton <xaviosx@gmail.com>
2014-02-07 17:45:43 -0700

    Add favicon.

--- tylers-code-base.go
+++ tylers-code-base.go
@@ -11,12 +11,18 @@ func main() {
 		panic(err)
 	}
 
+	favicon, icoErr := NewResponse("./public/favicon.ico")
+	if err != nil {
+		panic(icoErr)
+	}
+
 	sd, err := NewStaticDirectory("./public/", "/public/")
 	if err != nil {
 		panic(err)
 	}
 
 	http.Handle("/", homepage)
+	http.Handle("/favicon.ico", favicon)
 	http.Handle("/public/", sd)
 
 	http.ListenAndServe(":80", nil)
