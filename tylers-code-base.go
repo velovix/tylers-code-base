0146289945ca938fa0d18b2ecc6a1e114d2dde78
Tyler Compton <xaviosx@gmail.com>
2014-06-05 16:26:27 -0700

    Add view counter.

--- tylers-code-base.go
+++ tylers-code-base.go
@@ -1,10 +1,64 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"os"
 )
 
+var (
+	viewCount int
+)
+
+func addView(w http.ResponseWriter, r *http.Request) {
+
+	viewCount++
+	fmt.Fprintf(w, "%v", viewCount)
+	saveConfigFile()
+}
+
+func loadConfigFile() {
+
+	file, err := os.Open("./config.json")
+	if err != nil {
+		saveConfigFile()
+		loadConfigFile()
+		return
+	}
+	defer file.Close()
+	decoder := json.NewDecoder(file)
+
+	config := make(map[string]interface{})
+	err = decoder.Decode(&config)
+	if err != nil {
+		panic(err)
+	}
+
+	viewCount = int(config["viewCount"].(float64))
+}
+
+func saveConfigFile() {
+
+	file, err := os.Create("./config.json")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	encoder := json.NewEncoder(file)
+
+	config := make(map[string]interface{})
+	config["viewCount"] = viewCount
+
+	err = encoder.Encode(config)
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
+
+	loadConfigFile()
 
 	homepage, err := NewResponse("./views/index.html")
 	if err != nil {
@@ -26,6 +80,7 @@ func main() {
 		panic(err)
 	}
 
+	http.HandleFunc("/addView", addView)
 	http.Handle("/", homepage)
 	http.Handle("/docs", documentation)
 	http.Handle("/favicon.ico", favicon)
