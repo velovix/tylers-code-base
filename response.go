79eb96c2ab2bf1cc03abf88768214ae6270019d6
Tyler Compton <xaviosx@gmail.com>
2015-02-20 10:00:07 +0000

    Restructure server to use AppEngine.

--- response.go
+++ response.go
@@ -1,4 +1,4 @@
-package main
+package tylerscodebase
 
 import (
 	"fmt"
--- tylers-code-base.go
+++ tylers-code-base.go
@@ -1,95 +1,13 @@
-package main
+package tylerscodebase
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
-	"os"
-	"strconv"
 )
 
-const (
-	DefaultViewCount = 0
-	DefaultPort      = 3000
-)
-
-var (
-	viewCount int
-	port      int
-)
-
-func loadConfigFile() {
-
-	file, err := os.Open("./config.json")
-	if err != nil {
-		newConfigFile()
-		loadConfigFile()
-		return
-	}
-	defer file.Close()
-	decoder := json.NewDecoder(file)
-
-	config := make(map[string]interface{})
-	err = decoder.Decode(&config)
-	if err != nil {
-		panic(err)
-	}
-
-	viewCount = int(config["viewCount"].(float64))
-	port = int(config["port"].(float64))
-}
-
-func saveConfigFile() {
-
-	file, err := os.Create("./config.json")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer file.Close()
-	encoder := json.NewEncoder(file)
-
-	config := make(map[string]interface{})
-	config["viewCount"] = viewCount
-	config["port"] = port
-
-	err = encoder.Encode(config)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func newConfigFile() {
-
-	file, err := os.Create("./config.json")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer file.Close()
-	encoder := json.NewEncoder(file)
-
-	config := make(map[string]interface{})
-	config["viewCount"] = DefaultViewCount
-	config["port"] = DefaultPort
-
-	err = encoder.Encode(config)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func addView(w http.ResponseWriter, r *http.Request) {
-
-	viewCount++
-	fmt.Fprintf(w, "%v", viewCount)
-	saveConfigFile()
-}
-
-func main() {
+func init() {
 
 	log.SetFlags(log.Ldate | log.Ltime)
-
-	loadConfigFile()
 
 	homepage, err := NewResponse("./views/index.html")
 	if err != nil {
@@ -108,11 +26,8 @@ func main() {
 
 	docPage := DocPage{}
 
-	http.HandleFunc("/addView", addView)
 	http.Handle("/", homepage)
 	http.Handle("/docs/", docPage)
 	http.Handle("/favicon.ico", favicon)
 	http.Handle("/public/", sd)
-
-	http.ListenAndServe(":"+strconv.Itoa(port), nil)
 }
