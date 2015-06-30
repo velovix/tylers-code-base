9eb2d3e53756f70a976c4062e60081bf6f196cf4
Tyler Compton <xaviosx@gmail.com>
2015-06-30 00:31:02 -0700

    Add file and MIME caching for response object.

--- response.go
+++ response.go
@@ -1,35 +1,74 @@
 package tylerscodebase
 
 import (
-	"fmt"
 	"io/ioutil"
+	"log"
+	"mime"
 	"net/http"
-	"os"
-	"time"
+	"strings"
+	"sync"
+
+	"gopkg.in/fsnotify.v1"
 )
 
 type response struct {
-	file string
+	mutex    sync.Mutex
+	filename string
+	mimeType string
+	data     []byte
+	watcher  *fsnotify.Watcher
 }
 
-func newResponse(filename string) (response, error) {
+func newResponse(filename string) (*response, error) {
 
-	_, err := ioutil.ReadFile(filename)
+	var err error
+	var resp response
+
+	resp.data, err = ioutil.ReadFile(filename)
 	if err != nil {
-		return response{""}, err
+		return &response{}, err
+	}
+	resp.filename = filename
+
+	extension := strings.Split(resp.filename, ".")
+	if len(extension) > 0 {
+		resp.mimeType = mime.TypeByExtension(extension[len(extension)-1])
+	} else {
+		resp.mimeType = "text/plain"
 	}
 
-	return response{filename}, nil
+	resp.watcher, err = fsnotify.NewWatcher()
+	if err != nil {
+		return &response{}, err
+	}
+	err = resp.watcher.Add(filename)
+	if err != nil {
+		return &response{}, err
+	}
+	go func() {
+		for {
+			select {
+			case event := <-resp.watcher.Events:
+				resp.mutex.Lock()
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					log.Println("modified file:", resp.filename)
+				}
+				resp.mutex.Unlock()
+			case err := <-resp.watcher.Errors:
+				log.Println(err)
+			}
+		}
+	}()
+
+	return &resp, nil
 }
 
-func (resp response) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (resp *response) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
-	file, err := os.Open(resp.file)
-	if err != nil {
-		fmt.Fprint(w, "404 file error: %s", resp.file)
-		return
-	}
+	resp.mutex.Lock()
 
-	var zeroTime time.Time
-	http.ServeContent(w, r, resp.file, zeroTime, file)
+	w.Header().Add("Content-Type", resp.mimeType)
+	w.Write(resp.data)
+
+	resp.mutex.Unlock()
 }
