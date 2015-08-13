4888ce4ebf6004cf0834ad37008fac16f8bf6610
Tyler Compton <xaviosx@gmail.com>
2015-08-12 21:56:09 -0700

    Remove file caching because app engine doesn't support fsnotify. This is a sad day for everyone.

--- doc-page.go
+++ doc-page.go
@@ -6,9 +6,6 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"sync"
-
-	"gopkg.in/fsnotify.v1"
 )
 
 const (
@@ -17,10 +14,6 @@ const (
 )
 
 type documentation struct {
-	mutex        sync.Mutex
-	watcher      *fsnotify.Watcher
-	pageData     map[string]*[]byte
-	pageWatchers map[string]*fsnotify.Watcher
 }
 
 type docPageData struct {
@@ -29,90 +22,32 @@ type docPageData struct {
 }
 
 func newDocumentation() (*documentation, error) {
-
-	var err error
-	var doc documentation
-
-	// Start watching the doc template file
-	doc.watcher, err = fsnotify.NewWatcher()
-	if err != nil {
-		return &documentation{}, err
-	}
-	err = doc.watcher.Add(documentationFile)
-	if err != nil {
-		return &documentation{}, err
-	}
-	go func() {
-		for {
-			select {
-			case event := <-doc.watcher.Events:
-				doc.mutex.Lock()
-				if event.Op&fsnotify.Write == fsnotify.Write {
-					log.Println("modified file:", documentationFile)
-					// Clear the cache if the doc template has been modified
-					doc.pageData = make(map[string]*[]byte)
-				}
-				doc.mutex.Unlock()
-			case err := <-doc.watcher.Errors:
-				log.Println(err)
-			}
-		}
-	}()
-
-	doc.pageData = make(map[string]*[]byte)
-	doc.pageWatchers = make(map[string]*fsnotify.Watcher)
-
-	return &doc, nil
+	return &documentation{}, nil
 }
 
 func (doc *documentation) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	page := r.URL.Path[len("/docs/"):]
 
-	// Check if the page is already cached
-	if data, ok := doc.pageData[page]; ok {
-		w.Write(*data)
-	} else {
-		doc.mutex.Lock()
-
-		// Parse the generic documentation template
-		t, err := template.ParseFiles(documentationFile)
-		if err != nil {
-			log.Fatalln(err)
-		}
-
-		// Read the documentation data file
-		textData, err := ioutil.ReadFile(docDataLocation + page + ".html")
-		if err != nil {
-			textData, _ = ioutil.ReadFile(docDataLocation + "unknown.html")
-			page = "unknown"
-		}
-
-		// Create a struct to fill the template with
-		docPageData := docPageData{page, template.HTML(string(textData))}
-
-		// Save the completed page into the cache
-		doc.pageData[page] = new([]byte)
-		dataWriter := bytes.NewBuffer(make([]byte, 0))
-		t.Execute(dataWriter, docPageData)
-		*doc.pageData[page] = dataWriter.Bytes()
-
-		// Start watching the data file
-		doc.pageWatchers[page], err = fsnotify.NewWatcher()
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		err = doc.pageWatchers[page].Add("./public/docs/" + page + ".html")
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		go newFileUpdater(doc.pageWatchers[page], &doc.mutex, doc.pageData[page], "./public/docs/"+page+".html")()
-
-		// Return results
-		w.Write(*doc.pageData[page])
-
-		doc.mutex.Unlock()
+	// Parse the generic documentation template
+	t, err := template.ParseFiles(documentationFile)
+	if err != nil {
+		log.Fatalln(err)
 	}
+
+	// Read the documentation data file
+	textData, err := ioutil.ReadFile(docDataLocation + page + ".html")
+	if err != nil {
+		textData, _ = ioutil.ReadFile(docDataLocation + "unknown.html")
+		page = "unknown"
+	}
+
+	// Create a struct to fill the template with
+	docPageData := docPageData{page, template.HTML(string(textData))}
+
+	dataWriter := bytes.NewBuffer(make([]byte, 0))
+	t.Execute(dataWriter, docPageData)
+
+	// Return results
+	w.Write(dataWriter.Bytes())
 }
--- response.go
+++ response.go
@@ -5,56 +5,37 @@ import (
 	"mime"
 	"net/http"
 	"strings"
-	"sync"
-
-	"gopkg.in/fsnotify.v1"
 )
 
 type response struct {
-	mutex    sync.Mutex
 	filename string
-	mimeType string
-	data     []byte
-	watcher  *fsnotify.Watcher
 }
 
 func newResponse(filename string) (*response, error) {
 
-	var err error
 	var resp response
 
-	resp.data, err = ioutil.ReadFile(filename)
-	if err != nil {
-		return &response{}, err
-	}
 	resp.filename = filename
-
-	extension := strings.Split(resp.filename, ".")
-	if len(extension) > 0 {
-		resp.mimeType = mime.TypeByExtension(extension[len(extension)-1])
-	} else {
-		resp.mimeType = "text/plain"
-	}
-
-	resp.watcher, err = fsnotify.NewWatcher()
-	if err != nil {
-		return &response{}, err
-	}
-	err = resp.watcher.Add(filename)
-	if err != nil {
-		return &response{}, err
-	}
-	go newFileUpdater(resp.watcher, &resp.mutex, &resp.data, resp.filename)()
 
 	return &resp, nil
 }
 
 func (resp *response) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
-	resp.mutex.Lock()
+	data, err := ioutil.ReadFile(resp.filename)
+	if err != nil {
+		http.Error(w, "could not read file", 500)
+		return
+	}
 
-	w.Header().Add("Content-Type", resp.mimeType)
-	w.Write(resp.data)
+	var mimeType string
+	extension := strings.Split(resp.filename, ".")
+	if len(extension) > 0 {
+		mimeType = mime.TypeByExtension(extension[len(extension)-1])
+	} else {
+		mimeType = "text/plain"
+	}
+	w.Header().Add("Content-Type", mimeType)
 
-	resp.mutex.Unlock()
+	w.Write(data)
 }
